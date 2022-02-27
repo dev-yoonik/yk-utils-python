55e486d8dd5338fb7175162db0699258b2d59c16
Vitor Pedro <vitor@yoonik.me>
2022-02-27 12:01:12 +0000

    Add new class to store Face Authentication result information.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Objects module.
+- New class to store face authentication result data (instead of proprieties in FaceAuthentication class).
 
 ### Changed
 
--- yk_utils/apis/__init__.py
+++ yk_utils/apis/__init__.py
@@ -3,4 +3,4 @@ from .base_url import BaseUrl
 from .key import Key
 from .yoonik_api_exception import YoonikApiException
 from .http_requests import request
-from .face_authentication import FaceAuthentication
+from .face_authentication import FaceAuthentication, FaceAuthenticationResult
