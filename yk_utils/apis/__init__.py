df83847cb0f80fca6edccedcabfcace5877c906e
Vitor Pedro <vitor@yoonik.me>
2022-03-03 09:41:26 +0000

    Update __init__.py

--- yk_utils/apis/__init__.py
+++ yk_utils/apis/__init__.py
@@ -3,5 +3,5 @@ from .base_url import BaseUrl
 from .key import Key
 from .yoonik_api_exception import YoonikApiException
 from .http_requests import request
-from .face_authentication import FaceAuthentication
 from .api_result import ApiResult
+from .face_authentication import FaceAuthentication
