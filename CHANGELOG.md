f4c7da68f553e5d36c9625d05c77689475ed6158
João Ferreira <jppdpf@outlook.pt>
2022-06-29 17:05:59 +0100

    Added request async wrapper.
    Added httpx as requirement.
    Updated yk_utils package version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Request Async Wrapper
+
+## v1.2.0
+
+### Added
+
 - StrBaseEnum class
 
 ## v1.1.4
--- yk_utils/apis/__init__.py
+++ yk_utils/apis/__init__.py
@@ -2,6 +2,6 @@
 from .base_url import BaseUrl
 from .key import Key
 from .yoonik_api_exception import YoonikApiException
-from .http_requests import request
+from .http_requests import request, request_async
 from .api_result import ApiResult
 from .face_authentication import FaceAuthentication
