144c91957ea135be8928356b6cb415382316829c
Youverse <69840985+dev-yoonik@users.noreply.github.com>
2024-01-05 09:51:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="yk_utils",
-    version="1.3.5",
+    version="1.3.6",
     description="Youverse utils package for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
